8840a85cd421cd7b1e687615ea26d8093c983071
Abhisek Kumar <abhisekkumaranand@gmail.com>
2025-06-25 03:20:06 -0700

    Update axios.js
    correction on base url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "https://wisper-hv4y.onrender.com/api" : "/api";
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:3000/api"
+    : "https://wisper-hv4y.onrender.com/api";
+
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
